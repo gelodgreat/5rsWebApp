dbf6efae5186ea3b800e3d219cd246d52e30ba05
msigelodgreat <crumali@trends.com.ph>
2020-06-27 10:33:22 +0800

    Adjust AppTable

--- src/app/views/material-kit/tables/AppTable.jsx
+++ src/app/views/material-kit/tables/AppTable.jsx
@@ -11,6 +11,7 @@ const AppTable = () => {
   const path = window.location.pathname;
   const [data, setData] = useState([])
   const [columns, setColumns] = useState([])
+  const [loaded, setLoaded] = useState(false)
   const [title, setTitle] = useState("");
 
   useEffect(() => {
@@ -18,18 +19,23 @@ const AppTable = () => {
       if (path === "/material/table1") {
         setTitle("Reports")
         await getReports();
+        await setLoaded(true);
       } else if (path === "/material/table2") {
         setTitle("Repairs")
         await getRepairs();
+        await setLoaded(true);
       } else if (path === "/material/table3") {
         setTitle("Recommend")
         await getRecommend();
+        await setLoaded(true);
       } else if (path === "/material/table4") {
         setTitle("Recognize")
         await getRecognize();
+        await setLoaded(true);
       } else if (path === "/material/table5") {
         setTitle("React")
         await getReact();
+        await setLoaded(true);
       }
     }
     load()
@@ -38,7 +44,11 @@ const AppTable = () => {
   const getReports = async () => {
     try {
       const reports = await connection.get('api/v1/report')
-      setData(reports.data.data)
+      setColumns([{ title: "Category", field: "category", editable: 'never', },
+      { title: "Description", field: "description", },
+      { title: "Photo", field: "photo", editable: 'never', },
+      { title: "Created At", field: "createdAt", editable: 'never', }])
+      setData(reports.data.data);
       console.log(reports.data.data)
     } catch (error) {
       console.log(error)
@@ -48,6 +58,12 @@ const AppTable = () => {
   const getRepairs = async () => {
     try {
       const repair = await connection.get('api/v1/repair')
+      setColumns(
+        [{ title: "Category", field: "category", editable: 'never', },
+        { title: "Description", field: "description", },
+        { title: "Photo", field: "photo", editable: 'never', },
+        { title: "Created At", field: "createdAt", editable: 'never', }]
+      )
       setData(repair.data.data)
       console.log(repair.data.data)
     } catch (error) {
@@ -58,6 +74,13 @@ const AppTable = () => {
   const getRecommend = async () => {
     try {
       const recommend = await connection.get('api/v1/recommend')
+      setColumns([
+        { title: "Description", field: "description", },
+        { title: "Photo", field: "photo", editable: 'never', },
+        { title: "Subject", field: "subject", editable: 'never', },
+        { title: "Type", field: "type", editable: 'never', },
+        { title: "Created At", field: "createdAt", editable: 'never', }
+      ])
       setData(recommend.data.data)
       console.log(recommend.data.data)
     } catch (error) {
@@ -68,6 +91,13 @@ const AppTable = () => {
   const getRecognize = async () => {
     try {
       const recognize = await connection.get('api/v1/recognize')
+      setColumns(
+        [{ title: "Category", field: "category", editable: 'never', },
+        { title: "Description", field: "description", },
+        { title: "Photo", field: "photo", editable: 'never', },
+        { title: "Subject", field: "subject", editable: 'never', },
+        { title: "Created At", field: "createdAt", editable: 'never', }
+        ])
       setData(recognize.data.data)
       console.log(recognize.data.data)
     } catch (error) {
@@ -97,22 +127,27 @@ const AppTable = () => {
       </div>
 
       <SimpleCard title={title}>
-        <MaterialTable
-          title={title}
-          columns={[
-            { title: "Subject", field: "subject", editable: 'never', },
-            { title: "Description", field: "description", },
-            { title: "Photo", field: "photo", editable: 'never', },
-            { title: "User", field: "user", editable: 'never', },
-            { title: "Created At", field: "createdAt", editable: 'never', },
-          ]}
-          data={data}
-          options={{
-            grouping: true,
-            searchFieldAlignment: "right",
-            sorting: true
-          }}
-        />
+        {loaded
+          ? <MaterialTable
+            title={title}
+            columns={columns}
+            data={data}
+            options={{
+              grouping: true,
+              searchFieldAlignment: "right",
+              sorting: true
+            }}
+            detailPanel={(rowData) => {
+              console.log(rowData)
+              return (
+                <>
+                  <p>User: {rowData.user.name}</p>
+                  <p>Email: {rowData.user.email}</p>
+                </>
+              )
+            }}
+          /> : null}
+
       </SimpleCard>
     </div>
   );
